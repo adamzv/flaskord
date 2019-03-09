1ea56471d7fb2639de46ac8c92e2bbc3562bf185
Adam Zverka <adamzverka@hotmail.com>
2019-03-09 20:50:52 +0100

    Update ChannelService.js

--- src/services/ChannelService.js
+++ src/services/ChannelService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:5000',
+  baseURL: '/',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
