a9c6054febadd2ea06a00590e5d5a96f530d8ead
Adam Zverka <adamzverka@hotmail.com>
2019-03-09 17:39:41 +0100

    requirements.txt

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,7 +9,7 @@ Vue.config.productionTip = false
 Vue.use(
   new VueSocketIO({
     debug: true,
-    connection: 'http://127.0.0.1:5000/',
+    connection: 'http://${process.env.BASE_URL}', //'http://127.0.0.1:5000/',
     options: { path: '/socket.io/' }
   })
 )
